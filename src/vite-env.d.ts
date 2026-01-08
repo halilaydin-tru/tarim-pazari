ceaa49cf3375716f8e203dcfa68c0994d38255c6
Tarim Pazari <tarim@pazari.com>
2026-01-08 08:10:18 +0300

    Remove react-google-recaptcha completely - force rebuild

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,20 +9,3 @@ declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
 }
-
-declare module 'react-google-recaptcha' {
-  import React from 'react';
-  
-  interface ReCAPTCHAProps {
-    sitekey: string;
-    onChange?: (token: string | null) => void;
-    onExpired?: () => void;
-    onErrored?: () => void;
-    theme?: 'light' | 'dark';
-    type?: 'image' | 'audio';
-    tabindex?: number;
-  }
-  
-  const ReCAPTCHA: React.FC<ReCAPTCHAProps>;
-  export default ReCAPTCHA;
-}
