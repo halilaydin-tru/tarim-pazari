3f4e6c2afd7de05dd3634cd35fb8ac6ef7fd45f3
Tarim Pazari <tarim@pazari.com>
2026-01-08 17:10:39 +0300

    Remove compiled JS files - use TypeScript only

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { AuthProvider } from './context/AuthContext';
-import './index.css';
-import App from './App.tsx';
-createRoot(document.getElementById('root')).render(_jsx(StrictMode, { children: _jsx(GoogleOAuthProvider, { clientId: "YOUR_GOOGLE_CLIENT_ID", children: _jsx(AuthProvider, { children: _jsx(App, {}) }) }) }));
